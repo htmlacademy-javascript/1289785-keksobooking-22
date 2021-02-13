cae2b755cc1ee4253ca593ae546f5115f0ee7989
MariiaFish <mashuliafishka@gmail.com>
2021-02-13 11:53:16 +0200

    Delited console from main.js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ import {createAd, SIMILAR_ADS_COUNT} from './gen-arr.js';
 
 const similarAds = new Array(SIMILAR_ADS_COUNT).fill(null).map(() => createAd());
 
-console.log(similarAds);
+similarAds;
