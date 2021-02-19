9f4e90030c14efc05b96025f3533c8b71830274b
MariiaFish <mashuliafishka@gmail.com>
2021-02-19 14:58:34 +0200

    Renaned js/gen-arr.js js/gen-similar-add.js to js/gen-object-element.js js/gen-object.js

--- js/gen-object.js
+++ js/gen-object.js
@@ -4,9 +4,12 @@ const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'condit
 const TIME = ['12:00', '13:00', '14:00'];
 const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 const TITLE = ['Вилла в Греции на берегу моря, для большой семьи', 'Уютный домик в самом сердце Амстердама', 'Уютные аппартаменты для путешествующих с котом', 'Стильные аппартаменты в центре Милана'];
+const MIN_USER_AVATAR = 1;
+const MAX_USER_AVATAR = 8;
+const MIN_INT_VALUE = 1;
 const TRIGGER_VALUE_FIRST = 1;
 const TRIGGER_VALUE_SECOND = 4;
-const MIN_ONE = 1;
+const MIN_ARR_LENGTH = 1;
 const MIN_ARR_ELEMENT = 0;
 const PRICE = [2000, 3000, 5900, 12000, 6900, 1000];
 const TYPE = ['palace', 'flat', 'house', 'bungalow'];
@@ -55,28 +58,28 @@ const getAppartType = (type) => {
 };
 
 //Функция для avatara
-const getAvatar = () => {
-  return 'img/avatars/user0' + getRandomInt(1,8) + '.png';
+const getAvatar = (min, max) => {
+  return 'img/avatars/user0' + getRandomInt(min, max) + '.png';
 };
 
-
+// Создаем объект
 const createAd = () => {
   return {
     author: {
-      avatar: getAvatar(),
+      avatar: getAvatar(MIN_USER_AVATAR, MAX_USER_AVATAR),
     },
     offer: {
       title: getArrElement(TITLE, MIN_ARR_ELEMENT),
       address: getRandomСoordinate(MIN_X, MAX_X, DECIMAL_PLACE_X) + ' , ' +  getRandomСoordinate(MIN_Y, MAX_Y, DECIMAL_PLACE_Y),
       price: getArrElement(PRICE, MIN_ARR_ELEMENT),
       type: getArrElement(TYPE, MIN_ARR_ELEMENT),
-      rooms: getRandomInt(MIN_ONE, MAX_ROOMS),
-      guests: getRandomInt(MIN_ONE, MAX_GUESTS),
+      rooms: getRandomInt(MIN_INT_VALUE, MAX_ROOMS),
+      guests: getRandomInt(MIN_INT_VALUE, MAX_GUESTS),
       checkin: getArrElement(TIME, MIN_ARR_ELEMENT),
       checkout: getArrElement(TIME, MIN_ARR_ELEMENT),
-      features: getRandomArr(FEATURES, MIN_ONE),
+      features: getRandomArr(FEATURES, MIN_ARR_LENGTH),
       description: getArrElement(DESCRIPTION, MIN_ARR_ELEMENT),
-      photos: getRandomArr(PHOTOS, MIN_ARR_ELEMENT),
+      photos: getRandomArr(PHOTOS, MIN_ARR_LENGTH),
     },
     location: {
       x: getRandomСoordinate(MIN_X, MAX_X, DECIMAL_PLACE_X),
@@ -85,6 +88,6 @@ const createAd = () => {
   };
 };
 
-const createAdds = new Array(SIMILAR_ADS_COUNT).fill(null).map(() => createAd());
-
-export {createAdds, MAP_FLAT_TYPE, getAppartType, printRoomsGuests, printCheckinCheckout, printPhoto};
+const adObject = createAd();
+//todo: Это нормально, что у меня много констант со значение 1 или можно просто создать константу со занчение 1 и 0 и вставлять их там где они нужны?
+export {adObject, MAP_FLAT_TYPE, getAppartType, printRoomsGuests, printCheckinCheckout, printPhoto, SIMILAR_ADS_COUNT};
